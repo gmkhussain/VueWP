d95a4a233e6c672181cde1f7b272067236bac329
Developer <gmk.hussain@gmail.com>
2021-10-14 15:22:29 +0500

    Add custom layout and thier usage

---
+++ vueapp/src/views/frontend/pages/LoginPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="home">
+        <div class="container">
+            <h4>Login Page</h4>
+            <h5>Layout: LoginLayout ( without Header and Footer )</h5>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Login_Page"
+}
+</script>
