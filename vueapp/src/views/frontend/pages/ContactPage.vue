0a0352fdd0f193d6311e71a6bc152193c25032a4
Developer <gmk.hussain@gmail.com>
2021-10-14 21:05:03 +0500

    Contact form exception handling

--- vueapp/src/views/frontend/pages/ContactPage.vue
+++ vueapp/src/views/frontend/pages/ContactPage.vue
@@ -3,57 +3,61 @@
         <div class="container">
             <h1>Contact Page</h1>
             
-                <form @submit="onSubmit" method="post" class="customForm" id="myForm">
+        
+            <form @submit="onSubmit" method="post" class="customForm" id="myForm">
 
-                        <label>Your Name</label>
-                        <input type="text"
-                                class="form-control"
-                                v-model="post.yourName"
-                                placeholder="Please Enter Your Full Name" required />
+                    <label>Your Name</label>
+                    <input type="text"
+                            class="form-control"
+                            v-model="formInputs.yourName"
+                            placeholder="Please Enter Your Full Name" required />
 
-                        <label>Your Email</label>
-                        <input 
-                                type="email"
-                                class="form-control"
-                                v-model="post.yourEmail"
-                                placeholder="Please Enter Your Address" required />
+                    <label>Your Email</label>
+                    <input 
+                            type="email"
+                            class="form-control"
+                            v-model="formInputs.yourEmail"
+                            placeholder="Please Enter Your Address" required />
 
-                        <label>Subject</label>
-                        <input 
-                                type="text"
-                                class="form-control"
-                                name="Subj"
-                                v-model="post.yourSubject"
-                                placeholder="Please Enter Your Subject" required />
-                        
-                        <label>Message</label>
-                        <textarea 
-                                name="Msg"
-                                v-model="post.yourMessage"
-                                class="form-control"
-                                placeholder="Description..."></textarea>
-         
-                        <button class="btn btnCustom" id="postsubmit" >Submit Data</button>
+                    <label>Subject</label>
+                    <input 
+                            type="text"
+                            class="form-control"
+                            name="Subj"
+                            v-model="formInputs.yourSubject"
+                            placeholder="Please Enter Your Subject" required />
                     
-                    </form>
+                    <label>Message</label>
+                    <textarea 
+                            name="Msg"
+                            v-model="formInputs.yourMessage"
+                            class="form-control"
+                            placeholder="Description..."></textarea>
+        
+                    <button class="btn btnCustom" id="postsubmit" >Submit Data</button>
+                
+                </form>
+
+                <h2 v-if="formStatus"> {{formStatus}} </h2>
+
         </div>
     </section>
 </template>
 
 <script>
-// import axios from 'axios';
-import contactServices from '../../../services/contact/contact'
+import contactServices from '@/services/contact/contact'
 
 export default {
     name: 'Contact_Page',
     data() {
-        return{
-            post: {
+        return {
+            formInputs: {
                 yourName: 'Amoos',
                 yourEmail: 'info@me.now',
                 yourSubject: 'Welcome',
                 yourMessage: 'Something',
-            }
+            },
+            formStatus: "null"
         }
     },
      methods : {
@@ -62,25 +66,29 @@ export default {
             e.preventDefault();
 
             const emailBody = {
-                "yourName": this.post.yourName,
-                "yourEmail": this.post.yourEmail,
-                "yourSubject": this.post.yourSubject,
-                "yourMessage": this.post.yourMessage,
+                "yourName": this.formInputs.yourName,
+                "yourEmail": this.formInputs.yourEmail,
+                "yourSubject": this.formInputs.yourSubject,
+                "yourMessage": this.formInputs.yourMessage,
             };
 
-            // 2. Create a FormData object, and append each field to the object
+            // Create a FormData object, and append each field to the object
             const form = new FormData();
             for (const field in emailBody) {
                 form.append(field, emailBody[field]);
             }
 
 
-            let resp = await contactServices.sendEmail(6, form)
-            
-            if ( resp.status == 200 ) {
-                this.posts = resp.data;
-                console.log("You Logged In: ", resp.data)
-            }
+           try {
+                let resp = await contactServices.sendEmail(6, form)
+                if ( resp.status == 200 ) {
+                    this.posts = resp.data;
+                    this.formStatus = "Contact form submited!"
+                    console.log("You Logged In: ", resp.data)
+                }
+           } catch ( err ) {
+               this.formStatus = "Try agin!"
+           }
     
            }
        },
