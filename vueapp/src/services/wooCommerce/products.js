fb55b35249a7b394dcce6bab93ec0311a41b15c9
Developer <gmk.hussain@gmail.com>
2021-10-18 19:23:52 +0500

    pagination inital

--- vueapp/src/services/wooCommerce/products.js
+++ vueapp/src/services/wooCommerce/products.js
@@ -4,11 +4,23 @@ const CK = "ck_e23d2e0cdfced671b30dd3629332bfd1a9a3d64f";
 const CS = "cs_efc1cfbaf33f90dc5a95f8e59129c4d5c5471995";
 
 const PRODUCT_API_URL = "/wc/v3/products";
-
+let productLength = 0;
 
 export default {
-    listing() {
-        return axios.get( process.env.VUE_APP_API_BASE_URL + PRODUCT_API_URL+`?consumer_key=${CK}&consumer_secret=${CS}` )
+    all(){
+        let REQ = process.env.VUE_APP_API_BASE_URL + PRODUCT_API_URL+`?consumer_key=${CK}&consumer_secret=${CS}`;
+        console.log("URL", REQ )
+        console.log("productLength", productLength)
+        return axios.get( REQ )
+    },
+    listing( perPageLimit=1, pageNumber=1 ) {
+        let REQ = process.env.VUE_APP_API_BASE_URL + PRODUCT_API_URL+`?consumer_key=${CK}&consumer_secret=${CS}&per_page=${perPageLimit}&page=${pageNumber}`;
+        console.log("URL", REQ )
+        console.log("productLength", productLength)
+        return axios.get( REQ )
+    },
+    pagination( perPageLimit=4, totalProducts ) {
+        return totalProducts / perPageLimit;
     }
 }
 
--- vueapp/src/views/frontend/pages/products/ProductList.vue
+++ vueapp/src/views/frontend/pages/products/ProductList.vue
@@ -23,6 +23,17 @@
                     <router-link :to="'/' + product.slug">View</router-link>
 
                 </div>
+
+
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <li v-for="(pagi, index) in productsInfo.paginations" :key="index"
+                            class="page-item">
+                            <a class="page-link" href="#">{{index+1}}</a>
+                        </li>
+                    </ul>
+                </nav>
+
             </div>
        </div>
     </section>
@@ -37,7 +48,13 @@ export default {
     data() {
        return {
           products: [],
-          loading: false
+          loading: false,
+          productsInfo: {
+            totalProducts: 0,
+            currentPage: 1,
+            perPageProcductLimit: 3,
+            paginations: 0
+          }
        }
     },
     components: {
@@ -47,15 +64,40 @@ export default {
         async getProducts() {
             this.loading = true;
 
-            let resp = await WooProduct.listing();
-            console.log(resp)
+            let resp = await WooProduct.listing( this.productsInfo.perPageProcductLimit, this.productsInfo.currentPage);
+            console.log("resp", resp)
             this.products = resp.data;
+            this.loading = false; 
+
+        },
+        async paginatorFunc() {
+
+            let resp = await WooProduct.all();
+            console.log("pagi", resp.data.length)
             
-            this.loading = false;
+            this.productsInfo.totalProducts = resp.data.length
+
+            let pagiLimit = (this.productsInfo.totalProducts ) / this.productsInfo.perPageProcductLimit;
+
+            console.log(  (this.productsInfo.totalProducts ) + " ~ " + this.productsInfo.perPageProcductLimit )
+
+            let integr = Math.floor(pagiLimit), decimal = pagiLimit - integr;
+
+            let pagi = integr;
+            console.log({integr, decimal})
+            if(decimal !== 0 ) {
+                pagi=integr +1 
+            }  
+            
+            console.log({pagi} )
+            this.productsInfo.paginations = pagi;
+            
         }
+
     },
     mounted() {
         this.getProducts()
+        this.paginatorFunc()
     }
 }
 </script>
